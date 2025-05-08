0a3468efd2d103dc9cc3a03bdd01afc707400a10
ihanafy <mostafaelhanfy51@gmail.com>
2025-05-08 20:37:26 +0300

    make a folder for Entity Configuration and configure the Category model

---
+++ Bookstore.DataAccess/EntityConfigurations/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Bookstore.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookstore.DataAccess.EntityConfigurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(30);
+        }
+    }
+}
