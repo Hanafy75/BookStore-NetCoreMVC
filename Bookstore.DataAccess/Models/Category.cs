7cf6a75388439f929ab12664311eb86a95259108
ihanafy <mostafaelhanfy51@gmail.com>
2025-05-10 22:27:31 +0300

    adding client side validation

--- Bookstore.DataAccess/Models/Category.cs
+++ Bookstore.DataAccess/Models/Category.cs
@@ -6,8 +6,14 @@ namespace Bookstore.DataAccess.Models
     public class Category
     {
         public int Id { get; set; }
+
+
+        [MaxLength(30)]
         public string Name { get; set; } = null!;
+
+
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Range Must Be Between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
