90e8f5d3ecc1295638df42341cac8c7fcd67e2b8
ihanafy <mostafaelhanfy51@gmail.com>
2025-05-12 19:32:01 +0300

    Add Admin,Customer Area and add dropdown list that has category for now

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -4,8 +4,9 @@ using Bookstore.DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
-namespace BulkyWeb.Controllers
+namespace BulkyWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly ICategoryService _categoryService;
